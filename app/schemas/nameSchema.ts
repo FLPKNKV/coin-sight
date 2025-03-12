c7af54fcb609fc950157ed69142985869630b873
filipkenkov97 <filipkenkov@gmail.com>
2025-03-12 01:05:49 +0100

    made name validation schema description better

--- app/schemas/nameSchema.ts
+++ app/schemas/nameSchema.ts
@@ -1,7 +1,6 @@
 import { z } from "zod";
 
 export const nameValidationSchema = z.object({
-    firstName: z.string().min(2, "Please enter a first name").regex(/^[a-zA-Z\s]*$/, "First name should not contain special characters or numbers"),
-    lastName: z.string().min(2, "Please enter a last name").regex(/^[a-zA-Z\s]*$/, "Last name should not contain special characters or numbers"),
+    firstName: z.string().min(2, "Please enter a first name longer than 2 characters").regex(/^[a-zA-Z\s]*$/, "First name should not contain special characters or numbers"),
+    lastName: z.string().min(2, "Please enter a first name longer than 2 characters").regex(/^[a-zA-Z\s]*$/, "Last name should not contain special characters or numbers"),
 })
-
