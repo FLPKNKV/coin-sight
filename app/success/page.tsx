df7d7838e031eb5682327932bc5533293e841452
filipkenkov97 <filipkenkov@gmail.com>
2025-03-12 01:42:19 +0100

    automatic redirect to the dashboard

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -1,7 +1,18 @@
+"use client"
+
 import Image from "next/image"
-import React from "react"
+import React, { useEffect } from "react"
+import { useRouter } from "next/navigation"
 
 const Success = () => {
+    const route = useRouter();
+
+    useEffect(() => {
+        setTimeout(() => {
+            route.push('/dashboard')
+        },3000)
+    }, []) // no dependecy because i want this to run only once when the component mounts. there won't be another redirect to the dashboard after this.
+
     return (
         <div className='animate-fadeInSlide flex flex-col items-center justify-center h-screen'>
             <Image src='/success.svg' alt='Success' width={120} height={120} />
