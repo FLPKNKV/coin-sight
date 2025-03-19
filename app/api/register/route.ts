92b8dbccd3ebdc9ec39af90bd70ba5201893f933
filipkenkov97 <filipkenkov@gmail.com>
2025-03-19 21:28:53 +0100

    route fixed for password not required anymore on register page

---
+++ app/api/register/route.ts
@@ -0,0 +1,35 @@
+import { connect } from "../../db/database-config"
+import User from "../../db/models/userModel"
+import { NextRequest, NextResponse } from "next/server"
+
+connect().then(() => console.log("DB connected")).catch(error => console.log("DB Connection failed", error))
+
+export async function POST(request: NextRequest) {
+    try {
+        const { firstName, lastName, emailAddress } = await request.json()
+
+        const user = await User.findOne({ emailAddress })
+        if (user) {
+            return NextResponse.json({ error: "User already exists" }, { status: 400 })
+        }
+        const newUser = new User({
+            firstName,
+            lastName,
+            emailAddress
+        })
+        const savedUser = await newUser.save()
+
+        return NextResponse.json({
+            message: "User created successfully",
+            success: true,
+            savedUser,
+        })
+    } catch (error: any) {
+        return NextResponse.json(
+            {
+                error: error.message,
+            },
+            { status: 500 }
+        )
+    }
+}
