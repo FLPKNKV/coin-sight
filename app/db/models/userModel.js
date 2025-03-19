cb8347435f3622a989563a891456310db8cb3a70
filipkenkov97 <filipkenkov@gmail.com>
2025-03-19 17:11:21 +0100

    added db user model

---
+++ app/db/models/userModel.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose"
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: [true, "Please provide a first name"],
+    },
+    lastName: {
+        type: String,
+        required: [true, "Please provide a last name"],
+    },
+    emailAddress: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    isVerified: {
+        type: Boolean,
+        default: false,
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
+    forgotPasswordToken: String,
+    forgotPasswordTokenExpiry: Date,
+    verifyToken: String,
+    verifyTokenExpiry: Date,
+})
+
+const User = mongoose.models.users || mongoose.model("users", userSchema);
+
+export default User;
