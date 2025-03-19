16f67814a5b6cbeb181d00c331c2baec0beb49b6
filipkenkov97 <filipkenkov@gmail.com>
2025-03-19 01:10:24 +0100

    changed schema

--- app/schemas/userDetailSchema.ts
+++ app/schemas/userDetailSchema.ts
@@ -1,12 +1,17 @@
 import { z } from "zod";
 
-export const userDetailValidationSchema = z.object({
+export const userDetailValidationSchema = z
+  .object({
     firstName: z.string().min(2, "Please enter a first name longer than 2 characters").regex(/^[a-zA-Z\s]*$/, "First name should not contain special characters or numbers"),
-    lastName: z.string().min(2, "Please enter a first name longer than 2 characters").regex(/^[a-zA-Z\s]*$/, "Last name should not contain special characters or numbers"),
+    lastName: z.string().min(2, "Please enter a last name longer than 2 characters").regex(/^[a-zA-Z\s]*$/, "Last name should not contain special characters or numbers"),
     emailAddress: z.string().email("Please enter a valid email address"),
-    password: z.string().min(8, "Password must be at least 8 characters long"),
-    confirmPassword: z.string(),
-})
-.refine(data => data.password === data.confirmPassword, {
+  });
+
+export const userPasswordValidationSchema = z
+  .object({
+    password: z.string().min(8, "Password should be at least 8 characters long").regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/, "Password should contain at least one uppercase letter, one lowercase letter and one number"),
+    repeatPassword: z.string().min(8, "Password should be at least 8 characters long").regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/, "Password should contain at least one uppercase letter, one lowercase letter and one number"),
+  }).refine((data) => data.password === data.repeatPassword, {
     message: "Passwords do not match",
-});
+    path: ["repeatPassword"],
+  });
