54370993f6b36fae3a8710cb6539bffc442036c2
filipkenkov97 <filipkenkov@gmail.com>
2025-03-20 16:07:39 +0100

    changed name of app

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Register app",
+  title: "AI Coin Prediction",
   description: "Made by FK",
 };
 
