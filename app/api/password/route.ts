f0045fcddd056549dcc4cc735639ba0751f67e63
filipkenkov97 <filipkenkov@gmail.com>
2025-03-20 01:01:07 +0100

    backend validation for passwords

--- app/api/password/route.ts
+++ app/api/password/route.ts
@@ -4,11 +4,15 @@ import bcrypt from "bcryptjs";
 
 export async function POST(request: NextRequest) {
     try {
-        const { emailAddress, password } = await request.json();
+        const { emailAddress, password, repeatPassword } = await request.json();
         const user = await User.findOne({ emailAddress });
 
         if(!user){
             return NextResponse.json({ error: "User doesn't exist" }, { status: 400 });
+        }
+
+        if (password !== repeatPassword) {
+            return NextResponse.json({ error: "Passwords do not match" }, { status: 400 });
         }
 
         const salt = await bcrypt.genSalt(10)
