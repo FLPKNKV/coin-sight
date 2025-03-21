14588752944a663d381fd29e278f65973c9a83f2
filipkenkov97 <filipkenkov@gmail.com>
2025-03-21 23:21:16 +0100

    uped timeout for connecting to db

--- app/db/database-config.ts
+++ app/db/database-config.ts
@@ -2,7 +2,9 @@ import mongoose from 'mongoose';
 
 export async function connect (){
     try {
-        mongoose.connect(process.env.MONGODB_URI!);
+        mongoose.connect(process.env.MONGODB_URI!, {
+            serverSelectionTimeoutMS: 30000,
+        });
         const connection = mongoose.connection;
         
         connection.on('CONNECTED',  () => {
