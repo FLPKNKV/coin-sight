e013f6032d5f7736d51c76a4fd0d370da5cab3b7
filipkenkov97 <filipkenkov@gmail.com>
2025-03-21 21:38:53 +0100

    fixed tailwind config for deployment

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -21,8 +21,8 @@ export default {
     },
     keyframes: {
       fadeInSlide: {
-        '0%': { opacity: 0, transform: 'translateY(20px)' },
-        '100%': { opacity: 1, transform: 'translateY(0)' },
+        '0%': { opacity: "0", transform: 'translateY(20px)' },
+        '100%': { opacity: "1", transform: 'translateY(0)' },
       },
     },
     animation: {
