46f9e4e77f22dbc03081ab093aa20d61f7545732
filipkenkov97 <filipkenkov@gmail.com>
2025-03-21 23:31:38 +0100

    status added for creating successfuly a user

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -2,11 +2,12 @@ import { connect } from "../../db/database-config"
 import User from "../../db/models/userModel"
 import { NextRequest, NextResponse } from "next/server"
 
-connect().then(() => console.log("DB connected")).catch(error => console.log("DB Connection failed", error))
+connect()
+    .then(() => console.log("DB connected"))
+    .catch((error) => console.log("DB Connection failed", error))
 
 export async function POST(request: NextRequest) {
     try {
-
         const { firstName, lastName, emailAddress } = await request.json()
 
         const user = await User.findOne({ emailAddress })
@@ -16,19 +17,22 @@ export async function POST(request: NextRequest) {
         const newUser = new User({
             firstName,
             lastName,
-            emailAddress
+            emailAddress,
         })
         const savedUser = await newUser.save()
 
-        return NextResponse.json({
-            message: "User created successfully",
-            success: true,
-            savedUser,
-        })
+        return NextResponse.json(
+            {
+                message: "User created successfully",
+                success: true,
+                savedUser,
+            },
+            { status: 201 }
+        )
     } catch (error: unknown) {
         return NextResponse.json(
             {
-                error: (error instanceof Error) ? error.message : "An unknown error occurred",
+                error: error instanceof Error ? error.message : "An unknown error occurred",
             },
             { status: 500 }
         )
