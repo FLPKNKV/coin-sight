544591f00b0f271d7c8038058e7d980aed240438
filipkenkov97 <filipkenkov@gmail.com>
2025-03-22 03:10:58 +0100

    changed font in success page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -15,7 +15,7 @@ const Dashboard = () => {
   return (
     <div className="min-h-screen bg-gray-100 flex">
       <aside className="w-64 bg-white shadow-md p-5">
-        <h2 className="text-2xl font-bold mb-6">Dashboard</h2>
+        <h2 className="text-2xl font-spacemono font-bold mb-6">Dashboard</h2>
         <nav>
           {tabs.map((tab) => (
             <button
