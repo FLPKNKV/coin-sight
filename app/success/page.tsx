2e7c6cc4cfcbf6d30c8c57384ffc20650b694088
filipkenkov97 <filipkenkov@gmail.com>
2025-03-22 03:11:14 +0100

    changed success message font

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -14,10 +14,10 @@ const Success = () => {
     }, []) // no dependecy because i want this to run only once when the component mounts. there won't be another redirect to the dashboard after this.
 
     return (
-        <div className='animate-fadeInSlide flex flex-col items-center justify-center h-screen'>
+        <div className='animate-fadeInSlide flex flex-col items-center justify-center min-h-screen'>
             <Image src='/success.svg' alt='Success' width={120} height={120} />
-            <p className='font-garamond font-bold text-2xl'>Congratulations</p>
-            <p className='font-grotesk text-center text-sm duration-500 mt-2'>
+            <p className='font-spacemono font-bold text-2xl'>Congratulations</p>
+            <p className='font-spacemono text-center text-sm duration-500 mt-2'>
                 You have successfuly createad an account
             </p>
         </div>
