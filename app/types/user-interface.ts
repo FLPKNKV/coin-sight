f0eaabfd21ef2962497bde96531a9c970a58449d
filipkenkov97 <filipkenkov@gmail.com>
2025-03-27 03:54:03 +0100

    changed error type

--- app/types/user-interface.ts
+++ app/types/user-interface.ts
@@ -1,10 +1,10 @@
 import { AxiosError } from "axios"
 
-export interface User {
+export type User = {
     firstName?: string
     lastName?:string
     password: string
     repeatPassword?: string
     emailAddress?: string
-    error?: AxiosError
+    error?: AxiosError | unknown
 }
