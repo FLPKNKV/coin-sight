56b9ada348f1d5a9340b89a9153f33271dbfc042
filipkenkov97 <filipkenkov@gmail.com>
2025-03-29 20:09:44 +0100

    added dashboard component with shadcn

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next"
 import { ReactNode } from "react"
+import { ThemeProvider } from "@/components/theme-provider"
 import { connect } from "./db/database-config"
 import "./globals.css"
 
@@ -12,8 +13,20 @@ export default function RootLayout({ children }: { children: ReactNode }) {
     connect();
 
     return (
-        <html lang='en'>
-            <body>{children}</body>
+        <>
+        <html lang="en" suppressHydrationWarning>
+          <head />
+          <body>
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem
+              disableTransitionOnChange
+            >
+              {children}
+            </ThemeProvider>
+          </body>
         </html>
+      </>
     )
 }
